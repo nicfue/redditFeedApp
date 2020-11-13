6df6dc2e70962ca7f58b4d35683743030fc0029c
Nicolas@DESKTOP-K9DI919 <nicolasfuentes@hotmail.com>
2020-11-13 15:11:07 +0100

    deleted detectRoutingOnGoing

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,5 +1,5 @@
+import { Component, OnInit } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
-import { Component, Input, OnInit } from '@angular/core';
 import { LoadingService } from './loading.service';
 
 @Component({
@@ -9,31 +9,26 @@ import { LoadingService } from './loading.service';
 })
 export class LoadingComponent implements OnInit {
 
-  @Input()
-  detectRoutingOnGoing = false;
-
   constructor(public loadingService: LoadingService, private router: Router) { }
 
   ngOnInit(): void {
-    if (this.detectRoutingOnGoing) {
-      this.router.events
-        .subscribe(
-          event => {
-            if (
-              event instanceof NavigationStart ||
-              event instanceof RouteConfigLoadStart) {
-              this.loadingService.loadingOn();
-            }
-            else if (
-              event instanceof NavigationEnd ||
-              event instanceof NavigationError ||
-              event instanceof NavigationCancel ||
-              event instanceof RouteConfigLoadEnd) {
-              this.loadingService.loadingOff();
-            }
+    this.router.events
+      .subscribe(
+        event => {
+          if (
+            event instanceof NavigationStart ||
+            event instanceof RouteConfigLoadStart) {
+            this.loadingService.loadingOn();
           }
-        )
-    }
+          else if (
+            event instanceof NavigationEnd ||
+            event instanceof NavigationError ||
+            event instanceof NavigationCancel ||
+            event instanceof RouteConfigLoadEnd) {
+            this.loadingService.loadingOff();
+          }
+        }
+      )
   }
 
 }

103fd1c43bc55c590212614d0a9088934f4b7307
Nicolas@DESKTOP-K9DI919 <nicolasfuentes@hotmail.com>
2020-11-14 22:41:41 +0100

    display created in time ago instead of date

--- src/app/feeds/feeds-card-list/feeds-card-list.component.ts
+++ src/app/feeds/feeds-card-list/feeds-card-list.component.ts
@@ -22,4 +22,24 @@ export class FeedsCardListComponent {
     this.showFeedText = !this.showFeedText;
   }
 
+  feedCreated(feedCreated) {
+    const ONE_HOUR_IN_MILLISECONDS = 3600000, ONE_DAY_IN_MILLISECONDS = 86400000;
+
+    let currentTime = Date.now();
+    let timeDifference = currentTime - feedCreated;
+    const MINUTES_AGO = (timeDifference / 60) / 1000,
+      HOURS_AGO = (timeDifference / 60 / 60) / 1000,
+      DAYS_AGO = (timeDifference / 60 / 60) / 1000
+
+    let created: string;
+    if (timeDifference < ONE_HOUR_IN_MILLISECONDS) {
+      created = `${MINUTES_AGO} minutes ago`;
+    } else if (timeDifference >= ONE_HOUR_IN_MILLISECONDS && timeDifference <= ONE_DAY_IN_MILLISECONDS) {
+      created = `${Math.floor(HOURS_AGO)} hours ago`;
+    } else {
+      created = `${Math.floor(DAYS_AGO / 24)} days ago`
+    }
+    return created;
+  }
+
 }

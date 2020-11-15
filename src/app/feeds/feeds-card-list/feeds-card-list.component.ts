708bab12a2c18ccb98ef9c1aefc95753556ea9df
Nicolas@DESKTOP-K9DI919 <nicolasfuentes@hotmail.com>
2020-11-15 17:02:42 +0100

    refactoring

--- src/app/feeds/feeds-card-list/feeds-card-list.component.ts
+++ src/app/feeds/feeds-card-list/feeds-card-list.component.ts
@@ -22,31 +22,31 @@ export class FeedsCardListComponent {
     this.showFeedText = !this.showFeedText;
   }
 
-  feedCreated(feedCreated) {
-    const ONE_HOUR_IN_MILLISECONDS = 3600000,
-      ONE_DAY_IN_MILLISECONDS = 86400000,
-      ONE_MONTH_IN_MILLISECONDS = 2592000000,
-      ONE_YEAR_IN_MILLISECONDS = 31556952000
+  createdTimeAgo(created) {
+    const ONE_HOUR = 3600000,
+      ONE_DAY = 86400000,
+      ONE_MONTH = 2592000000,
+      ONE_YEAR = 31556952000
 
     let currentTime = Date.now();
-    let timeDifference = currentTime - (feedCreated * 1000);
+    let timeDifference = currentTime - (created * 1000);
     const MINUTES_AGO = (timeDifference / 60) / 1000,
-      HOURS_AGO = (timeDifference / 60 / 60) / 1000,
+      HOURS_AGO = (MINUTES_AGO / 60),
       DAYS_AGO = HOURS_AGO / 24,
-      MONTHS_AGO = ((timeDifference / 60 / 60 / 60) / 12) / 1000,
+      MONTHS_AGO = ((HOURS_AGO / 60) / 12),
       YEARS_AGO = MONTHS_AGO / 12
 
     let createdTimeAgo: string;
-    if (timeDifference < ONE_HOUR_IN_MILLISECONDS) {
+    if (timeDifference < ONE_HOUR) {
       createdTimeAgo = `${Math.floor(MINUTES_AGO)} minutes ago`;
-    } else if (timeDifference >= ONE_HOUR_IN_MILLISECONDS && timeDifference <= ONE_DAY_IN_MILLISECONDS) {
-      createdTimeAgo = `${Math.round(HOURS_AGO)} hours ago`;
-    } else if (timeDifference > ONE_DAY_IN_MILLISECONDS && timeDifference <= ONE_MONTH_IN_MILLISECONDS) {
-      createdTimeAgo = `${Math.round(DAYS_AGO)} days ago`;
-    } else if (timeDifference > ONE_MONTH_IN_MILLISECONDS && timeDifference <= ONE_YEAR_IN_MILLISECONDS) {
-      createdTimeAgo = `created ${Math.round(MONTHS_AGO)} months ago`
+    } else if (timeDifference >= ONE_HOUR && timeDifference < ONE_DAY) {
+      createdTimeAgo = `${Math.floor(HOURS_AGO)} hours ago`;
+    } else if (timeDifference >= ONE_DAY && timeDifference < ONE_MONTH) {
+      createdTimeAgo = `${Math.floor(DAYS_AGO)} days ago`;
+    } else if (timeDifference >= ONE_MONTH && timeDifference < ONE_YEAR) {
+      createdTimeAgo = `${Math.floor(MONTHS_AGO)} months ago`
     } else {
-      createdTimeAgo = `${Math.round(YEARS_AGO)} years ago`;
+      createdTimeAgo = `${Math.floor(YEARS_AGO)} years ago`;
     }
     return `created: ${createdTimeAgo}`;
   }
